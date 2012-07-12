93b7b9034805c55554973a60176d63bb81cfc0f9
izzza <izabela.kulakowska@ananastar.com>
2012-07-12 10:06:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iza::Application.config.secret_token = 'dec58c65ea26ebeabddf3c40e622174dbf17d274cc4fdc0d2fbffa021bb98873f652da686636b2938059943f9ea40ec6258a53e1196a51377ab535cc522ff75e'
