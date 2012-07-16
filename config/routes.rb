455fa99e8d60d3bd3aea653434f8886deced7358
izzza <izabela.kulakowska@ananastar.com>
2012-07-16 12:06:55 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Iza::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   resources :posts do
 	resources :comments
-  end 
+  end
 
   root :to => 'home#index'
    match '/rejestracja',  to: 'users#new'
    match 'home/strona',    to: 'home#strona'
   get "home/index"
-  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -60,7 +60,7 @@ Iza::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
