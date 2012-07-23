482aa38746bf13876a46d0f87b0382301633bf44
izzza <izabela.kulakowska@ananastar.com>
2012-07-23 16:42:40 +0200

    cos tam cos tam

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,7 @@
 module CommentsHelper
+
+  def find_user(id)
+    user = User.find(id)
+    return user
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :users
   attr_accessible :body, :commenter
-  validates :commenter, :presence => true
+  validates :commenter, :presence => false
   validates :body, :presence => true
   validates :user_id, :presence => true
 
